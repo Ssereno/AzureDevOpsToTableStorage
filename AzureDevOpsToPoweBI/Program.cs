4a01e4ec3357de1a16820b6b74076ee8e8ada866
snsereno <sergiosereno@criticalmanufacturing.com>
2024-12-01 18:55:12 +0000

    Add readme with details

--- AzureDevOpsToPoweBI/Program.cs
+++ AzureDevOpsToPoweBI/Program.cs
@@ -25,34 +25,44 @@ namespace AzureDevOpsToPowerBI
            
             Console.Write("Clean data before sync.");
 
-            if (await AzureTablesManager<UserStory>.TableExistsAsync(AppSettings.UserStoriesTableName))  
+            try
+            {
+                if (await AzureTablesManager<UserStory>.TableExistsAsync(AppSettings.UserStoriesTableName))  
                 await AzureTablesManager<UserStory>.DeleteAllEntitiesAsync(AppSettings.UserStoriesTableName);
 
-            if (await AzureTablesManager<TfsTasks>.TableExistsAsync(AppSettings.TasksTableName)) 
-                await AzureTablesManager<TfsTasks>.DeleteAllEntitiesAsync(AppSettings.TasksTableName);
+                if (await AzureTablesManager<TfsTasks>.TableExistsAsync(AppSettings.TasksTableName)) 
+                    await AzureTablesManager<TfsTasks>.DeleteAllEntitiesAsync(AppSettings.TasksTableName);
 
-            Console.Write("Data cleansing done. Prepare to get new data.");
+                Console.Write("Data cleansing done. Prepare to get new data.");
 
-            foreach(Tuple<string, string,string> tuple in appsProjects)
+                foreach(Tuple<string, string,string> tuple in appsProjects)
+                {
+                    var userStories = await UserStoriesManager.GetTfsUserStories(tuple.Item1, tuple.Item2);
+                    await AzureTablesManager<UserStory>.InsertIntoAzureTableBulkAsync(userStories,AppSettings.UserStoriesTableName);
+                    Console.Write($"User Stories for {tuple.Item1} Done. \n");
+
+                    var tasks = await TaskManager.GetTfsTasks(tuple.Item1, tuple.Item2);
+                    await AzureTablesManager<TfsTasks>.InsertIntoAzureTableBulkAsync(tasks,AppSettings.TasksTableName);
+                    Console.Write($"Tasks for {tuple.Item1} Done. \n");
+
+                    var areas = await AreaManager.GetAreas(tuple.Item1, tuple.Item2);
+                    await AzureTablesManager<Area>.UpsertIntoAzureTableAsync(areas,AppSettings.AreasTableName);
+                    Console.Write($"Area for {tuple.Item1} Done. \n");
+                    
+                    var iterations = await IterationManager.GetTfsIterations(tuple.Item1,tuple.Item3);
+                    await AzureTablesManager<Iteration>.UpsertIntoAzureTableAsync(iterations,AppSettings.IterationTableName);
+                    Console.Write($"Iterations for {tuple.Item1} Done. \n");
+
+                    var SprintCapacity = await SprintCapacityManager.GetCapacityAsync(tuple.Item1,tuple.Item3);
+                    await AzureTablesManager<SprintCapacity>.UpsertIntoAzureTableAsync(SprintCapacity,AppSettings.SprintCapacityTableName);
+                    Console.Write($"Sprint Capacity for {tuple.Item1} Done. \n");
+                }
+
+                Console.Write("All Done.");
+            }
+            catch(Exception e)
             {
-                var userStories = await UserStoriesManager.GetTfsUserStories(tuple.Item1, tuple.Item2);
-                await AzureTablesManager<UserStory>.InsertIntoAzureTableBulkAsync(userStories,AppSettings.UserStoriesTableName);
-                Console.Write($"User Stories for {tuple.Item1} Done. \n");
-
-                var tasks = await TaskManager.GetTfsTasks(tuple.Item1, tuple.Item2);
-                await AzureTablesManager<TfsTasks>.InsertIntoAzureTableBulkAsync(tasks,AppSettings.TasksTableName);
-                Console.Write($"Tasks for {tuple.Item1} Done. \n");
-
-                var areas = await AreaManager.GetAreas(tuple.Item1, tuple.Item2);
-                await AzureTablesManager<Area>.UpsertIntoAzureTableAsync(areas,AppSettings.AreasTableName);
-                Console.Write($"Area for {tuple.Item1} Done. \n");
-                
-                var iterations = await IterationManager.GetTfsIterations(tuple.Item1,tuple.Item3);
-                await AzureTablesManager<Iteration>.UpsertIntoAzureTableAsync(iterations,AppSettings.IterationTableName);
-                Console.Write($"Iterations for {tuple.Item1} Done. \n");
-
-                var SprintCapacity = await SprintCapacityManager.GetCapacityAsync(tuple.Item1,tuple.Item3);
-                await AzureTablesManager<SprintCapacity>.UpsertIntoAzureTableAsync(SprintCapacity,AppSettings.SprintCapacityTableName);
+                Console.Write(e.Message.ToString());
             }
         }
     }
--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-# AzureDevOps Project Report
-The goal of this project is to synchronise AzureDevOps workitems (User Stories, Task, Areas, Iterations) from various projects into a TableStorage to allow centralised analysis of multiple projects.
+# Introduction
+The goal of this project is synchronize a set of **AzureDevOps** workitems (*User Stories, Task, Areas, Iterations, Features, Bugs*) from multiple Azure DevOps projects in to TableStorage to enable centralized analysis of multiple projects using PowerBI dashboards.
 
-# TODO
+## Architecture
+
+### Basic Flow Diagram
+![Azure](/AzureDevOpsToPoweBI/Diagrams/Overview.jpg)
+
+### Application settings
+There are a set of basic parameters that need to be supplied to the application for it to work:
+
+```C#
+internal static string TfsUri { 
+    get {return "Azure DevOps URL";}
+}
+
+public static string PersonalAccessToken { 
+    get {return "PAT from Azure DevOps";}
+}
+
+public static string AzureStorageConnectionString { 
+    get {return "Azure Table connection string"; }
+}
+```
+
+### Project Metadata
+To specify the projects to be synchronized it is necessary (*at least for now*) to indicate the Project Name; Area Path and Team Name. 
+
+```C#
+ /// <summary>
+/// Main Class.
+/// </summary>
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Start Sincronization....");
+
+        // List of projects to get data.
+        // Project Name; Area Path, Team Name
+        var appsProjects = new List<Tuple<string,string,string>>{
+                Tuple.Create("Project_A","Project_A\\Apollo","Project A Team"),
+        };
+```
